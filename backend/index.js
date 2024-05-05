f4829e431c7f0a88d1f2c92370cb3270b1659674
rxp220401@darden.com <rxp220401@darden.com>
2024-05-05 17:02:35 +0530

    frontend initial setup

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,4 @@
 const express = require("express")
-const connect = require("./configs/db")
-const port = 10000;
 const app = express();
 
 const userRouter = require("./controllers/User.controller")
@@ -12,11 +10,4 @@ app.use("/", (req, res) => {
   res.send({value: "Kuch nhi"})
 });
 
-app.listen(port, async () => {
-  try {
-    await connect();
-    console.log('Connected successfully to server');
-  } catch (error) {
-    console.log(error)
-  }
-})
+module.exports = app
--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,12 @@
-// import { app } from "./index";
-// 
+const app = require('./index')
+const connect = require("./configs/db")
+const port = 10000;
+
+app.listen(port, async () => {
+  try {
+    await connect();
+    console.log('Connected successfully to server');
+  } catch (error) {
+    console.log(error)
+  }
+})
